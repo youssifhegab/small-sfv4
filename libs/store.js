92fe55318b96660b600bb1a2f42bfc8fcef1dd9f
youssifhegab <51842259+youssifhegab@users.noreply.github.com>
2021-08-22 11:17:53 +0200

    finished client side rendering using swr

--- libs/store.js
+++ libs/store.js
@@ -1,38 +1,17 @@
-const apiEndpoint = "https://graphql-sfv4.zyda.com/graphql"
-const subdomain = "demo-sfv4"
-export const query = `query ($subdomain: String!) {
-    store (subdomain: $subdomain) {
-      id
-      branchesCount
-      logoUrl
-      descriptionEn
-      faviconUrl
-      pageTitleEn
-      photoUrl
+
+
+import useSWR from "swr";
+import request from "graphql-request";
+
+const getStore = (query, variables) => 
+                        request("https://graphql-sfv4.zyda.com/graphql", query, variables)
+
+
+export const useStore = (query, variables)=> {
+    const {data, error} = useSWR([query, variables], getStore)
+
+    return {
+        data,
+        error
     }
-    categories(subdomain: $subdomain){
-        products{
-          descriptionEn
-          maxPrepTime
-          displayPrice
-          titleEn
-        }
-        photoUrl
-        restaurantId
-        titleEn
-        uuid
-      }
-  }`
-
-
-export function getStore() {
-    return fetch(apiEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({query, variables: {subdomain}})
-      })
-        .then(r => r.json())
-        .then(data => data.data);
 }
--- pages/index.js
+++ pages/index.js
@@ -3,53 +3,41 @@ import Image from 'next/image'
 import { Header } from '../components/Header'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/layout'
-// import logo from '../../sfv4/public'
-import { getStore, query } from '../libs/store'
-import useSWR from 'swr'
-import { useState } from 'react'
+import { useStore } from '../libs/store'
+import { gql, request } from 'graphql-request'
 
-// const querySWR = `query ($subdomain: String!) {
-//   categories(subdomain: $subdomain){
-//     products{
-//       descriptionEn
-//       maxPrepTime
-//       displayPrice
-//       titleEn
-//     }
-//     photoUrl
-//     restaurantId
-//     titleEn
-//     uuid
-//   }
-// }`
-
-
-
-
-export default function Home({query}) {
-  // const [searchQuery, setSearchQuery] = useState('')
-
-  // gettingBooks= (event)=>{
-  //   const search_query = event.target.value
-  //   setSearchQuery(()=>({query}))
-
-  //   if(search_query){
-  //     BooksAPI.search(query.trim(), 20).then((res)=>{
-  //       res.length > 0?setSearchQuery(()=>({newBooks: res})):
-  //       this.setSearchQuery(()=>({newBooks:[]}))
-  //     })
-  //   }else{
-  //     this.setSearchQuery(()=>({newBooks:[]}))
-  //   }
-
-  // }
+const subdomain = "demo-sfv4"
+const variables = {subdomain}
+const query = gql`query ($subdomain: String!) {
+    store (subdomain: $subdomain) {
+      id
+      branchesCount
+      logoUrl
+      descriptionEn
+      faviconUrl
+      pageTitleEn
+      photoUrl
+    }
+    categories(subdomain: $subdomain){
+        products{
+          descriptionEn
+          maxPrepTime
+          displayPrice
+          titleEn
+        }
+        photoUrl
+        restaurantId
+        titleEn
+        uuid
+      }
+  }`
 
 
-  // const result = getStore()
-  // console.log("store data: "+ JSON.stringify(getStore().store.id))
 
 
-  const {data, error} = useSWR(query, getStore)
+export default function Home() {
+
+  const {data, error} = useStore(query, variables)
   if (error) return <h1>Something went wrong!</h1>
   if (!data) return <h1>Loading...</h1>
   return (
@@ -63,40 +51,26 @@ export default function Home({query}) {
           alt={data.store.pageTitleEn} 
           height={100}
           width={100}/>
-        <h1 style={{display: 'inline', textAlign: 'center'}}>{data.store.pageTitleEn}</h1>
+        <h1>{data.store.pageTitleEn}</h1>
       </Header>
       <div style={{marginLeft: '20px'}}>
-        {data.categories.map((category)=>(
-          <div>
-            <h2>{category.titleEn}</h2>
-            <p>{category.products.map((product)=>(
-              <div>
-                <p>{product.titleEn}</p>
-                <p>{product.descriptionEn}</p>
-              </div>
-            ))}</p>
-          </div>
-          
-        ))}
+        <ol>
+          {data.categories.map(category => (
+            <li key={category.uuid}>
+              {category.titleEn}
+              <ul>
+                {category.products.map(product => (
+                  <li key={product.uuid}>
+                    {product.titleEn}
+                    <p>{product.descriptionEn}</p>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ol>
       </div>
-
-      
-      
     </Layout>
   )
 }
 
-// const searchFood(event){
-//   const query = event.target.value;
-//   if(query){
-
-//   }
-// }
-
-export async function getStaticProps() {
-  return {
-    props: {
-      query,
-    }
-  }
-}
